669d9826bf5a04133b8f7224e513a8a95e13cd03
Fredrik Fridborn <fredrik.fridborn@gmail.com>
2016-12-11 16:37:11 +0100

    geotif script updated

--- Code/tif/geotif.m
+++ Code/tif/geotif.m
@@ -1,18 +1,36 @@
 %%
 clear
 clc
+close all
 %%
-[x, R] = geotiffread('../../Data/dhm/0153359e_582245n_20160905T073406Z_dhm.tif');
+filenames = ['0152945e_582034n_20160905T073402Z_';
+    '0152947e_582246n_20160905T073405Z_';
+    '0152949e_582459n_20160905T073406Z_';
+    '0153357e_582033n_20160905T073402Z_';
+    '0153359e_582245n_20160905T073406Z_';
+    '0153401e_582458n_20160905T073406Z_';
+    '0153403e_582710n_20160905T073405Z_';
+    '0153809e_582032n_20160905T073406Z_';
+    '0153811e_582244n_20160905T073406Z_';
+    '0153813e_582456n_20160905T073406Z_';
+    '0153816e_582709n_20160905T073404Z_'
+    ]
+
+[rows,cols] = size(filenames)
+key.GTModelTypeGeoKey  = 1;  % Projected Coordinate System (PCS)
+key.GTRasterTypeGeoKey = 2;  % PixelIsPoint
+key.ProjectedCSTypeGeoKey = 32633;
+key.ProjLinearUnitsGeoKey = 9001;
+key.VerticalUnitsGeoKey = 9001;
+
 %%
-clc
-coordRefSysCode = 4326; %4326
-%26986, PCS_NAD83_Massachusetts Projected Coordinate System.
-%GeoKeyDirectoryTag =
-filename = 'map_squares';
-image = imread([filename '.png']);
-geotiffwrite([filename '.tif'],image,R, ...
-    'coordRefSysCode',coordRefSysCode);%, ...
-    %'GeoKeyDirectoryTag',info.GeoTIFFTags.GeoKeyDirectoryTag);
+for x = 1:rows
+    file = filenames(x,:)
+    cls_file = [file,'cls.tif']
+    ccls_file = [file,'ccls.tif']
+    png_file = [file,'ccls.png']
     
-    %'GeoKeyDirectoryTag',
-    %GeoKeyDirectoryTag);
+    [cls, R] = geotiffread(cls_file); 
+    ccls = imread(png_file);
+    geotiffwrite(ccls_file,image,R,'GeoKeyDirectoryTag',key);
+end
